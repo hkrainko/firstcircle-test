dd5b15d4a6c4641d05c0059d93696b38532dbe69
gary <>
2025-10-16 01:20:42 +0800

    Initialize Spring Boot project with Kotlin, Gradle, and essential configurations.

---
+++ src/main/kotlin/org/my/firstcircletest/FirstcircleTestApplication.kt
@@ -0,0 +1,11 @@
+package org.my.firstcircletest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstcircleTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<FirstcircleTestApplication>(*args)
+}
---
+++ src/test/kotlin/org/my/firstcircletest/FirstcircleTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.my.firstcircletest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstcircleTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
